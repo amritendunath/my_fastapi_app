1a0b3c048288960d68e16d54e7645c16ba655452
amritendunath <amritendunath@gmail.com>
2025-08-11 15:21:41 +0530

    6th_commit_final_test_workflow_automation

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.get("/")
 async def health_check():
-    return PlainTextResponse("Hello From Fastapi v4 and Successfully automated the backend workflow")
+    return PlainTextResponse("Hello From Fastapi v4 and Successfully automated the backend workflow Yoo")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000, reload = True)
